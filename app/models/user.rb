35a74b1998cc5f7b833e85d208d4bbf787c5ad2d
ckim80 <ckim80@chicagobooth.edu>
2017-06-06 13:40:48 -0500

    Message App Draft

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,6 @@ class User < ApplicationRecord
 
   validates :username, :presence => true, :uniqueness => true
 
+  acts_as_messageable
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -73,5 +73,9 @@ Rails.application.routes.draw do
   get "/my_likes", :controller => "users", :action => "likes"
 
 
+  resources :conversations do
+    resources :messages
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
